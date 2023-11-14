92d2af3786ad768dc23ca5656d4d93277fe39fc3
Shoaib <615shoaib@gmail.com>
2023-11-14 14:42:17 +0500

    ApiData

--- vite-project/src/Compnents/Edit.jsx
+++ vite-project/src/Compnents/Edit.jsx
@@ -1,17 +1,28 @@
-import React, { useEffect, useState } from 'react';
-
+import React, { useEffect, useState,useContext } from 'react';
+import { AppProvider } from "../Content/Api";
 const Edit = ({ val }) => {
  const [editItem,setEditItem]=useState("")
+ const {data,value,setValue,setData}=useContext(AppProvider)
 
 const  handleEdit=(id)=>{
     console.log(id)
+    setEditItem(id)
+}
+const handlesave =()=>{
+  const updatedData = data.map((item) =>
+  item.id === val.id ? { ...item, title: value } : item
+);
+setData(updatedData)
+setValue(" ")
+localStorage.setItem("jsonData",JSON.stringify(updatedData))
 }
   return (
     <>
     {
         editItem ? (
             <>
-            <input placeholder="title" value="editItem" onChange={()=>setEditItem(e.target.value)} />
+            <input placeholder="title" value={value} onChange={(e)=>setValue(e.target.value)} />
+            <button onClick={handlesave}>SaveEdit</button>
             </>
         ):(
             <>
