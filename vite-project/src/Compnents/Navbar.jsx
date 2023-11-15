143673ba4d9d775d9ce9b29fd87b124f02604602
Shoaib <615shoaib@gmail.com>
2023-11-15 11:30:30 +0500

    ApiData

--- vite-project/src/Compnents/Navbar.jsx
+++ vite-project/src/Compnents/Navbar.jsx
@@ -11,9 +11,7 @@ const Navbar = () => {
     <a className="navbar-brand">
     <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRFScYwNMn1LN1VgF3sqrZgZZ0Gspv73eITzg&usqp=CAU" alt="Bootstrap" width="40" height="34" />
     </a>
-    <form className="d-flex" role="search">
        <Search />
-    </form> 
     <Sorted />
   </div>
 </nav>
