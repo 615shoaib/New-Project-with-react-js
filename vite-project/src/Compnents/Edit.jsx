a8d2af8fc56095a1b631b1fd70313d8c127c5798
Shoaib <615shoaib@gmail.com>
2023-11-16 11:22:25 +0500

    ApiData

--- vite-project/src/Compnents/Edit.jsx
+++ vite-project/src/Compnents/Edit.jsx
@@ -1,62 +1,65 @@
-import React, { useEffect, useState, useContext } from "react";
+import React, { useState, useContext } from "react";
 import { AppProvider } from "../Content/Api";
+
 const Edit = ({ val }) => {
-  const [editItem, setEditItem] = useState("");
+  const [editItem, setEditItem] = useState(null);
   const { data, setData } = useContext(AppProvider);
-  const [title,setTitle]=useState("")
-  const [price,setPrice]=useState("")
-  const [category,setCategory]=useState("")
+  const [title, setTitle] = useState(val.title || "");
+  const [price, setPrice] = useState(val.price || "");
+  const [category, setCategory] = useState(val.category || "");
 
   const handleEdit = (id) => {
-    console.log(id);
     setEditItem(id);
   };
+
   const handlesave = () => {
     const updatedData = data.map((item) =>
-      item.id === val.id ? { ...item, title: title,category:category,price:price } : item
+      item.id === val.id ? { ...item, title, category, price } : item
     );
     setData(updatedData);
-    setTitle("");
-    setPrice("");
-    setCategory("");
-    setEditItem(null)
+    setEditItem(null);
     localStorage.setItem("jsonData", JSON.stringify(updatedData));
   };
 
-  const cancelEdit =()=>{
-    setEditItem(null)
-    setTitle(val.title || "")
+  const cancelEdit = () => {
+    setEditItem(null);
+    setTitle(val.title || "");
     setPrice(val.price || "");
     setCategory(val.category || "");
-  }
+  };
+
   return (
     <>
       {editItem ? (
         <>
-        <div>
-          <input
-            placeholder="title"
-            value={title}
-            onChange={(e) => setTitle(e.target.value)}
-            className="border-1 border-black"
-            style={{border:"1px solid #ccc"}}
-          />
-           <input
-            placeholder="price"
-            value={price}
-            onChange={(e) => setPrice(e.target.value)}
-            className="border-1 border-black"
-            style={{border:"1px solid #ccc"}}
-          />
-           <input
-            placeholder="Category"
-            value={category}
-            onChange={(e) => setCategory(e.target.value)}
-            className="border-1 border-black"
-            style={{border:"1px solid #ccc"}}
-          />
-          <button onClick={handlesave} className="btn btn-primary">SaveEdit</button>
-          <button onClick={cancelEdit} className="btn btn-secondary">Cancel</button>
+          <div>
+            <input
+              placeholder="title"
+              value={title}
+              onChange={(e) => setTitle(e.target.value)}
+              className="border-1 border-black"
+              style={{ border: "1px solid #ccc" }}
+            />
+            <input
+              placeholder="price"
+              value={price}
+              onChange={(e) => setPrice(e.target.value)}
+              className="border-1 border-black"
+              style={{ border: "1px solid #ccc" }}
+            />
+            <input
+              placeholder="Category"
+              value={category}
+              onChange={(e) => setCategory(e.target.value)}
+              className="border-1 border-black"
+              style={{ border: "1px solid #ccc" }}
+            />
+            <button onClick={handlesave} className="btn btn-primary">
+              SaveEdit
+            </button>
+            <button onClick={cancelEdit} className="btn btn-secondary">
+              Cancel
+            </button>
           </div>
         </>
       ) : (
